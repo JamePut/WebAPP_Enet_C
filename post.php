e8ee154a35db13f3f28415ceac654a340be97c4b
JamePut <dutchmillblu__@hotmail.com>
2022-08-31 10:53:33 +0700

    Day3

---
+++ post.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <style type=“text/css”>
+        p {
+            border: 1px solid black;
+            padding: 2px;
+            margin: 40px; 
+        }
+            </style>
+</head>
+<body>
+    <center>
+        ต้องการดูกระทู้หมายเลข <?php echo $_GET["id"]; ?>
+        <table style="border: 2px solid black; width: 40%;" align="center">
+            <tr><td style="background-color: #6cd2fe;" colspan="2">แสดงความคิดเห็น</td></tr>
+            <tr><td><input type="text" name="message" size="102"></td></tr>
+            <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+        </table><br>
+        <a href="index.html" target="_blank"> กลับไปหน้าหลัก </a></center><br>
+    </center>
+</body>
+</html>
---
+++ verify.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>verify</title>
+    <style type=“text/css”>
+        p {
+            border: 1px solid black;
+            padding: 2px;
+            margin: 40px;
+        }
+            </style>
+</head>
+<body>
+    <h1><center>Webboard JamePut</center></h1>
+    <hr>
+    <center>
+        เข้าสู่ระบบด้วย <br>
+        Login = <?php echo $_POST["login"]; ?> <br>
+        Password = <?php echo $_POST["password"]; ?>
+    </form>
+    </center>
+</body>
+</html>
